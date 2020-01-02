bfc1c4c82cea1cdc3a13db19b4fcc0780c7045ff
Edinei Corrêa de Menezes <edniei.menezes@gmail.com>
2020-01-02 14:07:35 -0300

    Sessão 15 - aula 146 concluida

---
+++ LojaVirtual/Models/Constants/SituacaoConstant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models.Constants
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1052:Static holder types should be Static or NotInheritable", Justification = "<Pending>")]
+    public class SituacaoConstant
+    {
+        public const string Ativo = "A";
+        public const string Inativo = "I";
+    }
+}
