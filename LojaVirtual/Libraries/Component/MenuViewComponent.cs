2ea8f1c3dbbff68076890dd9874c9ad07557cb31
Edinei Correa de Menezes <edinei.menezes@gmail.com>
2020-02-25 16:22:27 -0300

    189. [ESTUDO] Menu - ViewComponent - Parte 1 - finish

---
+++ LojaVirtual/Libraries/Component/MenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using LojaVirtual.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Component
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private ICategoriaRepository _categoriaRepository;
+
+        public MenuViewComponent(ICategoriaRepository CategoriaRepository)
+        {
+            _categoriaRepository = CategoriaRepository; 
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var ListaCategoria = _categoriaRepository.ObterTodasCategorias().ToList();
+            return View();
+        }
+    }
+}
